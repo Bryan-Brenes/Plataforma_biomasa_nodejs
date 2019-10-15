fb90f08a76320bb25dfeceb5c2dd7a5c348bb90a
Bryan Brenes <bryanbrenes1@gmail.com>
2019-10-15 00:02:05 -0600

    pantalla registro

--- routes/authUsuarios.js
+++ routes/authUsuarios.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const bodyParser = require('body-parser');
+
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 router.get('/', (req, res) => {
     res.render("login");
@@ -9,12 +12,20 @@ router.get('/login', (req, res) => {
     res.render("login");
 });
 
-router.post('/login', (req, res) => {
-    console.log(req.body);
+router.post('/login', urlencodedParser, (req, res) => {
+    //console.log(req.body.email);
+    // poner aquí la validación del usuario una vez que se busca en la BD
+    res.render('perfilUsuario');
 })
 
 router.get('/register', (req, res) => {
     res.render("register");
+});
+
+router.post('/register', urlencodedParser, (req, res) => {
+
+    // almacenar en la BD la información del usuario
+    res.render("perfilUsuario");
 });
 
 module.exports = router;
